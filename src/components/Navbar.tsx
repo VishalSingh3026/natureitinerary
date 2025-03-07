f0f1461a609c3f5e0f231e664aeb158f5632e507
VishalSingh3026 <121923704+VishalSingh3026@users.noreply.github.com>
2025-03-08 01:08:16 +0530

    ghvjjhv

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar = () => {
             <img 
               src={logo} 
               alt="TravelHowl Logo" 
-              className="w-40 h-25 object-contain"
+              className="text-2xl w-40 h-25 object-contain"
             />
           {/* </div> */}
         </Link>
